968393735c38099c09b4c1a8a27e10f122914e76
fred <iamfredng@gmail.com>
2014-09-01 00:46:39 +0800

    Initial Commit

---
+++ platform/macosx/amiffy3d/amiffy3d/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  amiffy3d
+//
+//  Created by 吴永烽 on 9/1/14.
+//  Copyright (c) 2014 amiffy.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
