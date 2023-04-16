43969526c25c272f0256edd3d53695093057a73a
iamfredng <iamfredng@outlook.com>
2023-04-16 14:44:24 +0800

    移除旧文件

--- amiffy/lua_bind.h
+++
@@ -1,6 +0,0 @@
-﻿#ifndef LUA_BIND_H_
-#define LUA_BIND_H_
-#include <stdbool.h>
-#include <lua/lua.h>
-bool bind_amiffy_modules( lua_State* L );
-#endif
--- amiffy/lua_print_stack.h
+++
@@ -1,7 +0,0 @@
-#ifndef LUA_PRINT_STACK_H_
-#define LUA_PRINT_STACK_H_
-#include <lua/lua.h>
-
-void print_stack(lua_State *L);
-
-#endif
