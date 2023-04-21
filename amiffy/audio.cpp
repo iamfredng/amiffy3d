a850ee99cd59421736cfba52d4cf1834069aa140
iamfredng <iamfredng@outlook.com>
2023-04-21 09:12:50 +0800

    something

---
+++ amiffy/audio.cpp
@@ -0,0 +1,23 @@
+#include "audio.h"
+
+using namespace irrklang;
+
+static ISoundEngine* engine;
+
+void open_audio_module()
+{
+    engine = createIrrKlangDevice();
+    if ( !engine ) return;   // error starting up the engine
+}
+
+void close_audio_module()
+{
+
+    engine->drop();
+}
+
+
+void play_audio( const char* fileName, bool loop )
+{
+    engine->play2D( fileName, loop );
+}
--- amiffy/audio.h
+++ amiffy/audio.h
@@ -1,8 +1,11 @@
 #ifndef AMIFFY_AUDIO_H_
 #define AMIFFY_AUDIO_H_
+#include <irrKlang.h>
 
 void open_audio_module();
 
 void close_audio_module();
+
+void play_audio( const char* fileName, bool loop );
 
 #endif
